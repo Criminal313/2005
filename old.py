47ef8c56bc9af368be1712fb98bdf59e00bf8c3a
Criminal313 <165898468+Criminal313@users.noreply.github.com>
2024-04-13 09:30:49 +0430

    Add files via upload
    rm -rf 2005
    git clone https://github.com/Criminal313/2005.git
    cd 2005
    git pull
    python old.py

--- old.py
+++ old.py
@@ -403,7 +403,7 @@ logo ="""
 \033[1;32m𝙁𝘼𝘾𝙀𝘽𝙊𝙊𝙆>>>\033[1;31m»»\033[1;32mMAFIA REYESS✓✓               
 \033[1;97m𝙒𝙃𝘼𝙏𝙎𝘼𝙋𝙋>>>\033[1;97m»»\033[1;97m+93783361438✓✓
 \033[1;32m𝙂𝙄𝙏𝙃𝙐𝘽>>>>>>>\033[1;31m»»\033[1;31mCrimanl313✓✓
-\033[1;31m𝙑𝙄𝙍𝙎𝙊𝙉>>>>>>>\033[1;31m»»\033[1;32m9.5✓✓   
+\033[1;31m𝙑𝙄𝙍𝙎𝙊𝙉>>>>>>>\033[1;31m»»\033[1;32m9.9✓✓   
 \033[1;32m𝙏𝙊𝙊𝙇 𝙎𝙏𝙐𝙏𝘼𝙎>>>>\033[1;31m»»\033[1;32m𝙁𝙍𝙀𝙀✓✓
 \033[1;35m𝙏𝙀𝙍𝙈𝙐𝙓 𝘼𝙇𝙇 𝙒𝙊𝙍𝙆𝙄𝙉𝙂 𝘾𝙊𝙈𝙈𝘼𝙉𝘿𝙎 𝘽𝙔 MAFIA ✓✓
 \033[1;32m««««««««««««««««««««««««««««««««««««««««««««««««"""
@@ -851,9 +851,10 @@ class Main:
     'authority': 'm.facebook.com',
     'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
     'accept-language': 'en-US,en;q=0.9',
-    # 'cookie': 'datr=3b3iZTYMXaN9VcWGqCpGZ2ec; sb=3b3iZdhRdQ5MGjXias9sY67r; ps_l=0; ps_n=0; vpd=v1%3B578x314x3.43505597114563; oo=v1; dpr=3.43505597114563; wl_cbv=v2%3Bclient_version%3A2458%3Btimestamp%3A1712483490; locale=en_US; m_pixel_ratio=3.7835533618927; c_user=100000268365288; xs=15%3ASsI3jPC7rUqd0Q%3A2%3A1712606969%3A-1%3A-1; fr=0ULMFGIF7cmGl1dcS.AWWz3_q-pBkH9dLjkmcHr2R1WRo.BmFC2j..AAA.0.0.BmFE77.AWW9kuX09pw; m_page_voice=100000268365288; wd=712x1308; x-referer=eyJyIjoiL2Jvb2ttYXJrcy8%2FcmVmPWRibCZwYWlwdj0wJmVhdj1BZmFxNlluZ1J2Sy1ic3BCT05SNFo5X0xTRGJnU2dGVldsY3VvcW16aU5HdUxYclZGcFREZHQ1M09wbkM2bzFWaGxJIiwiaCI6Ii9ib29rbWFya3MvP3JlZj1kYmwmcGFpcHY9MCZlYXY9QWZhcTZZbmdSdkstYnNwQk9OUjRaOV9MU0RiZ1NnRlZXbGN1b3FtemlOR3VMWHJWRnBURGR0NTNPcG5DNm8xVmhsSSIsInMiOiJtIn0%3D',
+    'cache-control': 'max-age=0',
+    # 'cookie': 'datr=wIAUZkfNZci5jRYJIygKSgKM; sb=wIAUZkUmq2yI1hEMHk_9KhiX; ps_l=0; ps_n=0; locale=en_US; wl_cbv=v2%3Bclient_version%3A2462%3Btimestamp%3A1712640548; vpd=v1%3B578x314x3.43505597114563; m_pixel_ratio=3.7835533618927; x-referer=eyJyIjoiL2Jvb2ttYXJrcy8%2FcmVmPTEwNCZwYWlwdj0wJmVhdj1BZmJGY0N6S2VVTnMtMzkwRzhNUDlTTGZwS2tPYnA1VE5WUVNrT1kwZ09rWkFQM2V6ZmxOV3pkM0dRNHQtUDBGZnhZIiwiaCI6Ii9ib29rbWFya3MvP3JlZj0xMDQmcGFpcHY9MCZlYXY9QWZiRmNDektlVU5zLTM5MEc4TVA5U0xmcEtrT2JwNVROVlFTa09ZMGdPa1pBUDNlemZsTld6ZDNHUTR0LVAwRmZ4WSIsInMiOiJtIn0%3D; wd=712x1308',
     'dpr': '2.75',
-    'referer': 'https://m.facebook.com/bookmarks/?ref=dbl&paipv=0&eav=Afaq6YngRvK-bspBONR4Z9_LSDbgSgFVWlcuoqmziNGuLXrVFpTDdt53OpnC6o1VhlI',
+    'referer': 'https://m.facebook.com/bookmarks/?ref=104&paipv=0&eav=AfbFcCzKeUNs-390G8MP9SLfpKkObp5TNVQSkOY0gOkZAP3ezflNWzd3GQ4t-P0FfxY',
     'sec-ch-prefers-color-scheme': 'light',
     'sec-ch-ua': '"Not_A Brand";v="8", "Chromium";v="120"',
     'sec-ch-ua-full-version-list': '"Not_A Brand";v="8.0.0.0", "Chromium";v="120.0.6099.116"',
@@ -865,7 +866,6 @@ class Main:
     'sec-fetch-mode': 'navigate',
     'sec-fetch-site': 'same-origin',
     'sec-fetch-user': '?1',
-    'service-worker-navigation-preload': '{"r":1012615515,"y":"m"}',
     'upgrade-insecure-requests': '1',
     'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
     'viewport-width': '980',
@@ -1313,9 +1313,10 @@ class Main:
     'authority': 'm.facebook.com',
     'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
     'accept-language': 'en-US,en;q=0.9',
-    # 'cookie': 'datr=3b3iZTYMXaN9VcWGqCpGZ2ec; sb=3b3iZdhRdQ5MGjXias9sY67r; ps_l=0; ps_n=0; vpd=v1%3B578x314x3.43505597114563; oo=v1; dpr=3.43505597114563; wl_cbv=v2%3Bclient_version%3A2458%3Btimestamp%3A1712483490; locale=en_US; m_pixel_ratio=3.7835533618927; c_user=100000268365288; xs=15%3ASsI3jPC7rUqd0Q%3A2%3A1712606969%3A-1%3A-1; fr=0ULMFGIF7cmGl1dcS.AWWz3_q-pBkH9dLjkmcHr2R1WRo.BmFC2j..AAA.0.0.BmFE77.AWW9kuX09pw; m_page_voice=100000268365288; wd=712x1308; x-referer=eyJyIjoiL2Jvb2ttYXJrcy8%2FcmVmPWRibCZwYWlwdj0wJmVhdj1BZmFxNlluZ1J2Sy1ic3BCT05SNFo5X0xTRGJnU2dGVldsY3VvcW16aU5HdUxYclZGcFREZHQ1M09wbkM2bzFWaGxJIiwiaCI6Ii9ib29rbWFya3MvP3JlZj1kYmwmcGFpcHY9MCZlYXY9QWZhcTZZbmdSdkstYnNwQk9OUjRaOV9MU0RiZ1NnRlZXbGN1b3FtemlOR3VMWHJWRnBURGR0NTNPcG5DNm8xVmhsSSIsInMiOiJtIn0%3D',
+    'cache-control': 'max-age=0',
+    # 'cookie': 'datr=wIAUZkfNZci5jRYJIygKSgKM; sb=wIAUZkUmq2yI1hEMHk_9KhiX; ps_l=0; ps_n=0; locale=en_US; wl_cbv=v2%3Bclient_version%3A2462%3Btimestamp%3A1712640548; vpd=v1%3B578x314x3.43505597114563; m_pixel_ratio=3.7835533618927; x-referer=eyJyIjoiL2Jvb2ttYXJrcy8%2FcmVmPTEwNCZwYWlwdj0wJmVhdj1BZmJGY0N6S2VVTnMtMzkwRzhNUDlTTGZwS2tPYnA1VE5WUVNrT1kwZ09rWkFQM2V6ZmxOV3pkM0dRNHQtUDBGZnhZIiwiaCI6Ii9ib29rbWFya3MvP3JlZj0xMDQmcGFpcHY9MCZlYXY9QWZiRmNDektlVU5zLTM5MEc4TVA5U0xmcEtrT2JwNVROVlFTa09ZMGdPa1pBUDNlemZsTld6ZDNHUTR0LVAwRmZ4WSIsInMiOiJtIn0%3D; wd=712x1308',
     'dpr': '2.75',
-    'referer': 'https://m.facebook.com/bookmarks/?ref=dbl&paipv=0&eav=Afaq6YngRvK-bspBONR4Z9_LSDbgSgFVWlcuoqmziNGuLXrVFpTDdt53OpnC6o1VhlI',
+    'referer': 'https://m.facebook.com/bookmarks/?ref=104&paipv=0&eav=AfbFcCzKeUNs-390G8MP9SLfpKkObp5TNVQSkOY0gOkZAP3ezflNWzd3GQ4t-P0FfxY',
     'sec-ch-prefers-color-scheme': 'light',
     'sec-ch-ua': '"Not_A Brand";v="8", "Chromium";v="120"',
     'sec-ch-ua-full-version-list': '"Not_A Brand";v="8.0.0.0", "Chromium";v="120.0.6099.116"',
@@ -1327,7 +1328,6 @@ class Main:
     'sec-fetch-mode': 'navigate',
     'sec-fetch-site': 'same-origin',
     'sec-fetch-user': '?1',
-    'service-worker-navigation-preload': '{"r":1012615515,"y":"m"}',
     'upgrade-insecure-requests': '1',
     'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
     'viewport-width': '980',
